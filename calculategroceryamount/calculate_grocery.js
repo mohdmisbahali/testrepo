ac4e64ff2e471a0a3dd615a8de0e8aafcd1abc8c
mohdmisbahali <mohdmisbahali01@gmail.com>
2026-03-17 09:23:31 -0400

    task completed

--- calculategroceryamount/calculate_grocery.js
+++ calculategroceryamount/calculate_grocery.js
@@ -7,7 +7,7 @@ grocery1=parseFloat(document.getElementById('grocery1').value);
 grocery2=parseFloat(document.getElementById('grocery2').value);
 grocery3=parseFloat(document.getElementById('grocery3').value);
 
-let amount = grocery1+grocery2+grocery3;
+let amount = grocery1 + grocery2 + grocery3;
 
-document.getElementById('result').innerText = The total amount is :$($amount);
+document.getElementById('result').innerText = `The total Amount is: $${amount}`;
 }
